ed2ac44cfdf79e2029968ac360de823cf0caacc0
Maria Jesus <marychuartaviasol2606@gmail.com>
2023-08-20 13:43:38 -0600

    inicio del repositorio
    se ingresa todos los archivos del proyecto hotel

---
+++ src/hoteltrestristestigres/HotelTresTristesTigres.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hoteltrestristestigres;
+
+/**
+ *
+ * @author María Jesús
+ */
+public class HotelTresTristesTigres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public  void Hotel() {
+        // TODO code application logic here
+    }
+    
+}
